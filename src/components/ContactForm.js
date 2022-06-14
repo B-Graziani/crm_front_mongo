e0f0c5801d2d923efc3ec66e576c3dd55a9fe4dc
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-06-14 19:39:43 +0200

    form contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-// import ContactService from "../services/contact";
+// import Contact from "../models/contact";
 
 const ContactForm = () => {
   return (
     <div className="form">
       <h1>Formulaire</h1>
-      <div className="champA">ZKGEFGZUYERFUOZ</div>
-      <div className="champB">DGRETTR</div>
-      <div className="champc">RTHERTYH</div>
-      <div className="champD">RTHERTYHERT</div>
+      <div className="form-group">
+        <label htmlFor="agencyName">AgencyName</label>
+        <input id="agencyName" type="text" className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">name</label>
+        <input id="name" type="text" className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="surname">Surname</label>
+        <input id="surname" type="text" className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="mail">Mail</label>
+        <input id="mail" type="text" className="form-control"></input>
+      </div>
+      <div className="card-action center">
+        {/* Submit button */}
+        <button type="submit" className="btn">
+          Valider
+        </button>
+      </div>
       <div className="retour">
         <Link to={"/contacts"}>Retour</Link>
       </div>
